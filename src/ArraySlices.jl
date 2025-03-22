d69ff8f9e115d43a3a459193abd4ed1d13901f30
Thomas Burton <tburton5572@gmail.com>
2025-03-22 18:37:46 +0000

    generalised slice struct construction code to take indexing dimensions as tuple as well as int (not tested)

--- src/ArraySlices.jl
+++ src/ArraySlices.jl
@@ -1,54 +1,62 @@
-__precompile__()
+# __precompile__()
 module ArraySlices
 
 import Base: length, size, eltype, getindex, Slice, OneTo
 
 export slices, columns, rows
 
+# TODO: overload printing methods
 # Type parameters
 # 
 # F : type of SubArray
-# D : indexed dimension of the array
+# D : tuple of indexed dimensions of the array
+# N : number of indexed dimensions
 # A : array type
-# 
-struct SliceIterator{F, D, A<:AbstractArray} <: AbstractVector{F}
+struct SliceIterator{F, D, N, A<:AbstractArray} <: AbstractArray{F, N}
     array::A
 end
 
-# ensure compatibility between versions
-if VERSION < v"0.5-dev"
-    _get_L(D, N) = D == 1 ? N : D
-else
-    _get_L(D, N) = D == 1 || D == N ? true : false
-end
+"""
+    slices(array, dims)
+
+Return a `SliceIterator` object to loop over the slices of `array` along the
+dimensions `dims`. 
 
 """
-    slices(array, dim)
-
-Return a `SliceIterator` object to loop over the slices of `array` along
-dimension `dim`. 
-
-"""
-function slices(array::AbstractArray{T, N}, ::Type{Val{D}}) where {T, N, D}
+function slices(array::AbstractArray{T, M}, ::Val{D}) where {T, M, D}
     # checks
-    1 <= D <= N || error("invalid slice dimension")
+    prod(1 .<= D .<= M) || throw(ArgumentError("invalid slice dimension"))
 
     # construct type of slice
     # example: SubArray{Float64, 1, Array{Float64,2}, Tuple{Int64, Colon}, true}
-    F = SubArray{T, N-1, typeof(array), Tuple{[j == D ? Int : Slice{OneTo{Int64}} for j = 1:N]...}, _get_L(D, N)}
-    
+    N = length(D)
+    # TODO: do I even need to this type parameter, since it doesn't provide any new information and isn't used in the interface
+    F = SubArray{T, M - N, typeof(array), Tuple{[j ∈ D ? Int : Slice{OneTo{Int64}} for j = 1:M]...}, _get_L(D, M)}
+
     # build and return iterator
-    SliceIterator{F, D, typeof(array)}(array)
+    SliceIterator{F, D, length(D), typeof(array)}(array)
+end
+
+_get_L(D::Int, M) = D == 1 || D == M ? true : false
+function _get_L(D::Tuple, M)
+    slice_inds = filter(x -> !(x ∈ D), 1:M)
+    for i in 1:(length(slice_inds) - 1)
+        if slice_inds[i] != slice_inds[i + 1] - 1
+            return false
+        end
+    end
+    return true
 end
 
 # ~~~ Array interface ~~~
 eltype(s::SliceIterator{F}) where {F} = F
-length(s::SliceIterator{F, D}) where {F, D} = size(s.array, D)
-size(s::SliceIterator) = (length(s), )
+size(s::SliceIterator{F, D, 1}) where {F, D} = (size(s.array, D),)
+size(s::SliceIterator{F, D}) where {F, D} = map(dim -> size(s.array, dim), D)
+length(s::SliceIterator) = prod(size(s))
 
 # build code that produces slices with the correct indexing
-@generated function getindex(s::SliceIterator{F, D, A}, i::Integer) where {F, D, A}
-    args = [j == D ? :(i) : :(Colon()) for j = 1:ndims(A)]
+@generated function getindex(s::SliceIterator{F, D, N, A}, i::Integer) where {F, D, N, A}
+    args = [j ∈ D ? :(i) : :(Colon()) for j = 1:ndims(A)]
     return :(view(s.array, $(args...)))
 end
 
@@ -61,7 +69,7 @@ end
 Return a `SliceIterator` object to loop over the columns of `array`.
 
 """
-columns(array::AbstractMatrix) = slices(array, Val{2})
+columns(array::AbstractMatrix) = slices(array, Val(2))
 
 """
     rows(array)
@@ -69,6 +77,6 @@ columns(array::AbstractMatrix) = slices(array, Val{2})
 Return a `SliceIterator` object to loop over the rows of `array`.
 
 """
-rows(array::AbstractMatrix) = slices(array, Val{1})
+rows(array::AbstractMatrix) = slices(array, Val(1))
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,28 +2,32 @@ using Test
 
 using ArraySlices
 
+# TODO: add tests for more varied slice dimension options
 @testset "Slices                " begin
     # check constructor
     for nd = 2:5
         X = randn(rand(1:10, nd)...)
-        @test_throws ErrorException slices(X, Val{0})
-        @test_throws ErrorException slices(X, Val{nd+1})
+        @test_throws ArgumentError slices(X, Val(0))
+        @test_throws ArgumentError slices(X, Val(nd+1))
     end
+    # X = randn(rand(1:10, 5)...)
+    # @show typeof(slices(X, Val(2)))
+    # @show typeof(slices(X, Val((1, 3))))
 
     # check length and size of slices
     X = randn(1, 2, 3, 4, 5, 6)
     for i = 1:ndims(X)
-        @test length(slices(X, Val{i})) == i
-        @test size(slices(X, Val{i})) == (i, )
+        @test length(slices(X, Val(i))) == i
+        @test size(slices(X, Val(i))) == (i, )
     end
 
     # check compatibility with views
     X = randn(5, 5, 5, 5, 5)
-    @test typeof(view(X, 1, :, :, :, :)) == eltype(slices(X, Val{1}))
-    @test typeof(view(X, :, 1, :, :, :)) == eltype(slices(X, Val{2}))
-    @test typeof(view(X, :, :, 1, :, :)) == eltype(slices(X, Val{3}))
-    @test typeof(view(X, :, :, :, 1, :)) == eltype(slices(X, Val{4}))
-    @test typeof(view(X, :, :, :, :, 1)) == eltype(slices(X, Val{5}))
+    @test typeof(view(X, 1, :, :, :, :)) == eltype(slices(X, Val(1)))
+    @test typeof(view(X, :, 1, :, :, :)) == eltype(slices(X, Val(2)))
+    @test typeof(view(X, :, :, 1, :, :)) == eltype(slices(X, Val(3)))
+    @test typeof(view(X, :, :, :, 1, :)) == eltype(slices(X, Val(4)))
+    @test typeof(view(X, :, :, :, :, 1)) == eltype(slices(X, Val(5)))
 end
 
 @testset "Row and column methods" begin
